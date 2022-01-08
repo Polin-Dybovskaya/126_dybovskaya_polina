9e8b6d722e0b6b817b129ce697ab5df20a730e77
Polin-Dybovskaya <90262722+Polin-Dybovskaya@users.noreply.github.com>
2022-01-08 15:41:45 +0300

    создан тест (раб)create_CreateCompanyDocs_ContractsCountEqualsZero

---
+++ task_10/src/Company_docs_core/CompanyDocs.java
@@ -0,0 +1,10 @@
+package Company_docs_core;
+
+public class CompanyDocs {
+    public int getContractsCount(){
+        return 0;
+    }
+    public static CompanyDocs create(){
+        return new CompanyDocs();
+    }
+}
--- task_10/src/Company_docs_tests/CompanyDocsTest.java
+++ task_10/src/Company_docs_tests/CompanyDocsTest.java
@@ -1,6 +1,13 @@
 package Company_docs_tests;
 
+import Company_docs_core.CompanyDocs;
 import org.junit.*;
 
+
 public class CompanyDocsTest extends Assert{
+    @Test
+    public void create_CreateCompanyDocs_ContractsCountEqualsZero() {
+        CompanyDocs companyDocs = CompanyDocs.create();
+        assertEquals(0, companyDocs.getContractsCount());
+    }
 }
