f21cfb1f015d0ab4a3047ee0f9973600c7ad5dcf
Polin-Dybovskaya <90262722+Polin-Dybovskaya@users.noreply.github.com>
2022-01-08 17:53:23 +0300

    исправление бага заново создан create_CreateCompanyDocs_ContractsCountEqualsZero

--- task_10/src/Company_docs_tests/CompanyDocsTest.java
+++ task_10/src/Company_docs_tests/CompanyDocsTest.java
@@ -1,19 +1,13 @@
 package Company_docs_tests;
 
-import Company_docs_core.*;
+import Company_docs_core.CompanyDocs;
 import org.junit.*;
 
-//
+
 public class CompanyDocsTest extends Assert{
     @Test
     public void create_CreateCompanyDocs_ContractsCountEqualsZero() {
         CompanyDocs companyDocs = CompanyDocs.create();
-        assertEquals(0, companyDocs.getContractsCount());
-    }
-    @Test
-    public void addContract_AddContractWithNumberAndDate_ContractsCountEqualsOne() {
-        CompanyDocs companyDocs = CompanyDocs.create();
-        companyDocs.addContract("1","20220108");
         assertEquals(0, companyDocs.getContractsCount());
     }
 }
