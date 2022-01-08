b0e8d5d0b75859890b9202c622f1452c9efc3b78
Polin-Dybovskaya <90262722+Polin-Dybovskaya@users.noreply.github.com>
2022-01-08 19:43:48 +0300

    add addContract_AddContractsWithNumberAndData_ContractsCountEqualsOne

--- task_10/src/Company_docs_tests/CompanyDocsTest.java
+++ task_10/src/Company_docs_tests/CompanyDocsTest.java
@@ -2,11 +2,17 @@ package Company_docs_tests;
 
 import Company_docs_core.CompanyDocs;
 import org.junit.*;
-//
+
 public class CompanyDocsTest extends Assert{
     @Test
     public void create_CreateCompanyDocs_ContractsCountEqualsZero() {
         CompanyDocs companyDocs = CompanyDocs.create();
         assertEquals(0, companyDocs.getContractsCount());
+    }
+    @Test
+    public void addContract_AddContractsWithNumberAndData_ContractsCountEqualsOne() {
+        CompanyDocs companyDocs = CompanyDocs.create();
+        companyDocs.addContract ("number","data)";
+        assertEquals(1, companyDocs.getContractsCount());
     }
 }
