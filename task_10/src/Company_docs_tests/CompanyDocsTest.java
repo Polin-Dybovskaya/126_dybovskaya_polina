e88cf2188a01f076da1eb365a14dd987ad4e54e6
Polin-Dybovskaya <90262722+Polin-Dybovskaya@users.noreply.github.com>
2022-01-10 23:05:06 +0300

    rename -> CompanyContracts

--- task_10/src/Company_docs_tests/CompanyDocsTest.java
+++ task_10/src/Company_docs_tests/CompanyDocsTest.java
@@ -1,38 +1,39 @@
 package Company_docs_tests;
 
-import Company_docs_core.CompanyDocs;
+import Company_docs_core.*;
 import org.junit.*;
 
 public class CompanyDocsTest extends Assert {
     @Test
     public void create_CreateCompanyDocs_ContractsCountEqualsZero() {
-        CompanyDocs companyDocs = CompanyDocs.create();
+        CompanyContracts companyDocs = CompanyContracts.create();
         assertEquals(0, companyDocs.getContractsCount());
     }
 
     @Test
     public void addContract_AddContractsWithNumberAndData_ContractsCountEqualsOne() {
-        CompanyDocs companyDocs = CompanyDocs.create();
+        CompanyContracts companyDocs = CompanyContracts.create();
         companyDocs.addContract("number", "date");
         assertEquals(1, companyDocs.getContractsCount());
     }
 
     @Test
     public void addContract_AddContractsWithNullNumber_ThrowsException() {
-        CompanyDocs companyDocs = CompanyDocs.create();
+        CompanyContracts companyDocs = CompanyContracts.create();
         var exc = assertThrows(IllegalArgumentException.class, () -> companyDocs.addContract(null, "date"));
-        assertTrue(exc.getMessage().toLowerCase().contains("number can't be null"));
+        assertTrue(exc.getMessage().toLowerCase().contains("Number cannot be null"));
     }
     @Test
     public void addContract_AddContractsWithNullData_ThrowsException() {
-        CompanyDocs companyDocs = CompanyDocs.create();
+        CompanyContracts companyDocs = CompanyContracts.create();
         var exc = assertThrows(IllegalArgumentException.class, () -> companyDocs.addContract("number",null));
-        assertTrue(exc.getMessage().toLowerCase().contains("date can't be null"));
+        assertTrue(exc.getMessage().toLowerCase().contains("Date cannot be null"));
     }
     @Test
     public void addContract_AddContractsWithNullDataAndNullNumber_ThrowsException() {
-        CompanyDocs companyDocs = CompanyDocs.create();
+        CompanyContracts companyDocs = CompanyContracts.create();
         var exc = assertThrows(IllegalArgumentException.class, () -> companyDocs.addContract(null,null));
-        assertTrue(exc.getMessage().toLowerCase().contains("number and date cannot be null"));
+        assertTrue(exc.getMessage().toLowerCase().contains("number and date can't be null"));
     }
+
 }
