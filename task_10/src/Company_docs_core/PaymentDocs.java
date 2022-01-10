cd1ea089af2deb8072dabdf25bf3ae0f81d02bc6
Polin-Dybovskaya <90262722+Polin-Dybovskaya@users.noreply.github.com>
2022-01-11 00:30:22 +0300

    test completed.

---
+++ task_10/src/Company_docs_core/PaymentDocs.java
@@ -0,0 +1,13 @@
+package Company_docs_core;
+
+public class PaymentDocs {
+    private int sum;
+    private DocType docType;
+    private int date;
+
+    public PaymentDocs (int sum, DocType docType, int date){
+        this.sum = sum;
+        this.docType = docType;
+        this.date = date;
+    }
+}
