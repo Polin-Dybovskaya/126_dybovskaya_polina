ceafbc80798f26fe4d1b918f8f93ac676c876fa6
Polin-Dybovskaya <90262722+Polin-Dybovskaya@users.noreply.github.com>
2022-01-11 01:01:09 +0300

    add getSum_getSumOfPaymentsOfContract_sumEqualsZero and getSum_getSumOfPaymentsOfContract_sumEquals1000 с исключениями

--- task_10/src/Company_docs_tests/ContractTest.java
+++ task_10/src/Company_docs_tests/ContractTest.java
@@ -18,15 +18,33 @@ public class ContractTest extends Assert {
         assertEquals(1, ListOfContracts.getContractsCount());
     }
     @Test
-    public void registerPaymentDocument_RegisterDocumentToContract_DocumentRegistered() {
-        ContractManager ListOfContracts = ContractManager.create();
-        ListOfContracts.addContract(1, 20220101);
-        ListOfContracts.addContract(2, 20770202);
-        ListOfContracts.registerPaymentDocument(100, 1, DocType.BankOrder, 1, 20220110);
-        ListOfContracts.registerPaymentDocument(200, 1, DocType.PaymentOrder, 2, 20770211);
-        assertEquals(1, ListOfContracts.getContractsList().get(2).getDocumentsListSize());
+    public void registerPaymentDocument_RegisterDocumentToContract_DocumentRegistered() throws Exception {
+            ContractManager ListOfContracts = ContractManager.create();
+            ListOfContracts.addContract(1, 20220110);
+            ListOfContracts.addContract(2, 20220111);
+            ListOfContracts.registerPaymentDocument(100, 1, DocType.BankOrder, 1, 20220110);
+            ListOfContracts.registerPaymentDocument(200, 1, DocType.PaymentOrder, 2, 20220111);
+            assertEquals(1, ListOfContracts.getContractsList().get(1).getDocumentsListSize());
+            assertEquals(1, ListOfContracts.getContractsList().get(2).getDocumentsListSize());
+
+        }
+        @Test
+        public void getSum_getSumOfPaymentsOfContract_sumEqualsZero () throws Exception {
+            ContractManager ListOfContracts = ContractManager.create();
+            ListOfContracts.addContract(1, 20220110);
+            assertEquals(0, ListOfContracts.getContractsList().get(1).getSumOfPayments());
 
     }
-
-
+    @Test
+    public void getSum_getSumOfPaymentsOfContract_sumEquals1000() throws Exception {
+        ContractManager ListOfContracts = ContractManager.create();
+        ListOfContracts.addContract(1, 20220101);
+        ListOfContracts.registerPaymentDocument(500, 1, DocType.BankOrder, 1, 20220110);
+        ListOfContracts.registerPaymentDocument(500, 1, DocType.BankOrder, 1, 20220110);
+        ListOfContracts.addContract(2, 20220101);
+        ListOfContracts.registerPaymentDocument(900, 1, DocType.BankOrder, 2, 20220110);
+        ListOfContracts.registerPaymentDocument(100, 1, DocType.BankOrder, 2, 20220110);
+        assertEquals(1000, ListOfContracts.getContractsList().get(1).getSumOfPayments());
+        assertEquals(1000, ListOfContracts.getContractsList().get(2).getSumOfPayments());
+    }
 }
