1849e4ac154b5a26215552fc84b1cea4b1a3b78b
Polin-Dybovskaya <90262722+Polin-Dybovskaya@users.noreply.github.com>
2022-03-18 20:14:09 +0300

    createArray_CreateArrayWithoutSize_SizeEqualsDefaultSize

--- 2sem/hw/task_1/src/Task1_Tests.java
+++ 2sem/hw/task_1/src/Task1_Tests.java
@@ -1,2 +1,19 @@
-public class Task1_Tests {
+import org.junit.*;
+
+public class Task1_Tests extends Assert {
+
+    private int getIntValue(ListNode<Integer> item) {
+        return ((Node<Integer>) item).getData();
+    }
+//DynamicArray...................
+//.............
+
+    @Test
+    public void  createArray_CreateArrayWithoutSize_SizeEqualsDefaultSize(){
+        DynamicArray<Integer> array = new DynamicArray<>();
+        assertEquals(1024, array.getSize());
+    }
+
 }
+
+
