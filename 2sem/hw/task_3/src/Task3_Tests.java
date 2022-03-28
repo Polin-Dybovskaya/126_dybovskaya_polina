0315c486559b0299977cd3ff21f08b52e098db9f
Polin-Dybovskaya <90262722+Polin-Dybovskaya@users.noreply.github.com>
2022-03-29 00:40:40 +0300

    task_3

--- 2sem/hw/task_3/src/Task3_Tests.java
+++ 2sem/hw/task_3/src/Task3_Tests.java
@@ -88,12 +88,61 @@ public class Task3_Tests extends Assert {
         assertArrayEquals(new Integer[]{1,1,3,4,4}, arr);
     }
     @Test
-    public void HeapSort_sortArrayB_SortCorrect() {
+    public void HeapSort_sortArrayB_SortCorrect() {//34 38
         Integer[] arr = new Integer[]{5, 1, 2, 3, 1, 5, 5, 2};
         HeapSort<Integer> hs = new HeapSort();
         hs.sort(arr, new TestComparator());
         assertArrayEquals(new Integer[]{1, 1, 2, 2, 3, 5, 5, 5}, arr);
     }
     //...........MergeSort.............
+    @Test
+    public void MergeSort_SortNotNeed_SortCorrect(){//w
+        Integer[] arr= {1,1,1};
+        MergeSort<Integer> qs = new MergeSort();
+        qs.sort(arr, new TestComparator());
+        assertArrayEquals(new Integer[]{1,1,1}, arr);
+    }
+    @Test
+    public void MergeSort_sortArrayWithTwoNumbers_SortCorrect() {// 35 19
+        Integer[] arr = new Integer[]{5, 3};
+        MergeSort<Integer> qs = new MergeSort();
+        qs.sort(arr, new TestComparator());
+        assertArrayEquals(new Integer[]{3, 5}, arr);
+    }
+    @Test
+    public void MergeSort_sortTwoNumbers_SortCorrect(){
+        Integer[] arr = new Integer[]{2,1,2,3};
+        MergeSort mergeSort = new MergeSort();
+        mergeSort.sort(arr, new TestComparator());
+        assertArrayEquals(new Integer[]{1, 2,2,3}, arr);
+    }
+    @Test
+    public void MergeSort_sortArrayWithSameNumbers_SortCorrect(){
+        Integer[] arr = {2,4,1,3,1};
+        MergeSort<Integer> qs = new MergeSort();
+        qs.sort(arr, new TestComparator());
+        assertArrayEquals(new Integer[]{1,1,2,3,4}, arr);
+    }
 
+    @Test
+    public void MergeSort_SortNotNeed_NumbersOnRightPositions(){//32
+        Integer[] arr = {1,2,3,4,5,6};
+        MergeSort<Integer>bs= new MergeSort<>();
+        bs.sort(arr,new TestComparator());
+        assertArrayEquals(new Integer[]{1,2,3,4,5,6}, arr);
+    }
+    @Test
+    public void MergeSort_sortArrayB_SortCorrect() {//18
+        Integer[] arr = new Integer[]{5, 1, 2, 3, 1, 5, 5, 2};
+        MergeSort<Integer> hs = new MergeSort();
+        hs.sort(arr, new TestComparator());
+        assertArrayEquals(new Integer[]{1, 1, 2, 2, 3, 5, 5, 5}, arr);
+    }
+    @Test
+    public void MergeSort_sortArray_SortCorrect(){//34
+        Integer[] arr = {4,1,1,3,4};
+        MergeSort<Integer> qs = new MergeSort();
+        qs.sort(arr, new TestComparator());
+        assertArrayEquals(new Integer[]{1,1,3,4,4}, arr);
+    }
 }
